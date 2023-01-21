1bae5e696bf845bb2afa1772033d7fd67a2c2fcd
Krishna Jade <krishnajade222@gmail.com>
2023-01-21 10:57:38 +0530

    Ask for location to turn on if disabled

--- app/src/main/java/com/trifrnd/registration/MainActivity.java
+++ app/src/main/java/com/trifrnd/registration/MainActivity.java
@@ -1,20 +1,26 @@
 package com.trifrnd.registration;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  {
+    private static final int LOCATION_PERMISSION_REQUEST =1 ;
     private EditText usernameEditText;
     private EditText passwordEditText;
     Context context;
@@ -23,16 +29,36 @@ public class MainActivity extends AppCompatActivity  {
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
+    private void enableLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location services not enabled");
+            builder.setMessage("Please enable location services");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            }).setNegativeButton("No Thanks", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+           AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        enableLocation();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         usernameEditText = findViewById(R.id.activity_main_usernameEditText);
         passwordEditText = findViewById(R.id.activity_main_passwordEditText);
-
-        Button registerButton = findViewById(R.id.activity_main_registerButton);
-        Button forgotPasswordButton = findViewById(R.id.activity_main_forgotPasswordButton);
         Button loginButton = findViewById(R.id.activity_main_loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,25 +70,26 @@ public class MainActivity extends AppCompatActivity  {
                 boolean condition3= isNetworkConnected();
                 if(condition3){
                     if ( condition1 && condition2 ) {
-                    Intent intent=new Intent(getApplicationContext(),AdminDashboard.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    String toastMessage = "Wrong Username or Password or you are not connected to network";
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-                }
+                        Intent intent=new Intent(getApplicationContext(),AdminDashboard.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        String toastMessage = "Wrong Username or Password ";
+                        Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                    }
                 }else{
                     String toastMessage = "You are not connected to network";
                     Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
                 }
             }
         });
+        Button registerButton = findViewById(R.id.activity_main_registerButton);
+        Button forgotPasswordButton = findViewById(R.id.activity_main_forgotPasswordButton);
         registerButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent (getApplicationContext(), RegistrationActivity.class);
                 startActivity(intent);
-
             }
         });
         forgotPasswordButton.setOnClickListener(new View.OnClickListener(){
@@ -70,7 +97,6 @@ public class MainActivity extends AppCompatActivity  {
             public void onClick(View view) {
                 Intent intent = new Intent (getApplicationContext(), ForgotPasswordActivity.class);
                 startActivity(intent);
-
             }
         });
     }
