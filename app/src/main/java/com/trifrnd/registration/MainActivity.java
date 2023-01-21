5208bc13933ee0d66f2640f88982479dd89c3584
Krishna Jade <krishnajade222@gmail.com>
2023-01-21 14:45:42 +0530

    User can only login if location is enabled

--- app/src/main/java/com/trifrnd/registration/MainActivity.java
+++ app/src/main/java/com/trifrnd/registration/MainActivity.java
@@ -12,23 +12,47 @@ import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
-    private static final int LOCATION_PERMISSION_REQUEST =1 ;
+public class MainActivity extends AppCompatActivity {
+    private static final int LOCATION_PERMISSION_REQUEST = 1;
     private EditText usernameEditText;
     private EditText passwordEditText;
     Context context;
+
     public boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
+
+    public boolean isLocationEnabled() {
+        int locationMode;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        } else {
+            locationProviders = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
     private void enableLocation() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
@@ -40,18 +64,23 @@ public class MainActivity extends AppCompatActivity  {
                 public void onClick(DialogInterface dialogInterface, int i) {
                     Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                     startActivity(intent);
+                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        finish();
+                    }
                 }
             }).setNegativeButton("No Thanks", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.cancel();
+                    // dialogInterface.cancel();
+                    String toastMessage = "You need to enable location to proceed to login";
+                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
                 }
             });
-           AlertDialog alertDialog = builder.create();
+            AlertDialog alertDialog = builder.create();
             alertDialog.show();
         }
     }
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         enableLocation();
@@ -63,21 +92,28 @@ public class MainActivity extends AppCompatActivity  {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user="user";
-                String password="password";
+                String user = "user";
+                String password = "password";
                 boolean condition1 = usernameEditText.getText().toString().equals(user);
                 boolean condition2 = passwordEditText.getText().toString().equals(password);
-                boolean condition3= isNetworkConnected();
-                if(condition3){
-                    if ( condition1 && condition2 ) {
-                        Intent intent=new Intent(getApplicationContext(),AdminDashboard.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        String toastMessage = "Wrong Username or Password ";
-                        Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                boolean condition3 = isNetworkConnected();
+                boolean condition4 = isLocationEnabled();
+                if (condition3) {
+                    if (condition4) {
+                        if (condition1 && condition2) {
+                            Intent intent = new Intent(getApplicationContext(), AdminDashboard.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            String toastMessage = "Wrong Username or Password ";
+                            Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }else{
+                    else{
+                        String toastMessage = "Turn on your location before login";
+                        Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();}
+
+                } else {
                     String toastMessage = "You are not connected to network";
                     Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
                 }
@@ -85,17 +121,17 @@ public class MainActivity extends AppCompatActivity  {
         });
         Button registerButton = findViewById(R.id.activity_main_registerButton);
         Button forgotPasswordButton = findViewById(R.id.activity_main_forgotPasswordButton);
-        registerButton.setOnClickListener(new View.OnClickListener(){
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (getApplicationContext(), RegistrationActivity.class);
+                Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
                 startActivity(intent);
             }
         });
-        forgotPasswordButton.setOnClickListener(new View.OnClickListener(){
+        forgotPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (getApplicationContext(), ForgotPasswordActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
                 startActivity(intent);
             }
         });
